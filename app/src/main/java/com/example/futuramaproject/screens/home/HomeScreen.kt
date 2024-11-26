0d2a548a51247b591e0816f69c2173bd5468fdee
Bruno Freitas <bruno.araujo@madeinweb.app>
2024-11-25 23:14:19 -0300

    feat: added button init in home

--- app/src/main/java/com/example/futuramaproject/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/futuramaproject/screens/home/HomeScreen.kt
@@ -1,12 +1,17 @@
 package com.example.futuramaproject.screens.home
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -23,6 +28,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,6 +37,7 @@ import androidx.compose.ui.window.DialogProperties
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.futuramaproject.R
+import com.example.futuramaproject.Screen
 import com.example.futuramaproject.components.LoadImage
 
 @Composable
@@ -55,16 +62,30 @@ fun AppBar() {
 fun ContentPage(navHostController: NavHostController, paddingValues: PaddingValues) {
     var showDialog by rememberSaveable { mutableStateOf(false) }
 
-    Box(
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(paddingValues)
             .background(MaterialTheme.colorScheme.primary),
-        contentAlignment = Alignment.Center
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
         LoadImage(imageResId = R.drawable.futurama, size = 200.dp, onClick = {
             showDialog = true
         })
+
+        Button(
+            modifier = Modifier
+                .padding(top = 50.dp)
+                .fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Black,
+            ),
+            shape = RectangleShape,
+            onClick = { navHostController.navigate(Screen.Detail.route) },
+        ) {
+            Text(color = Color.White, text = "Iniciar")
+        }
 
         if (showDialog) {
             FullScreenImageDialog(onDismiss = { showDialog = false })
