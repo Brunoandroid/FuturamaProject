7e0073cc871197137039eacfca5fcbb18b101d52
Bruno Freitas <bruno.araujo@madeinweb.app>
2024-11-25 22:45:26 -0300

    feat: image home with preview

--- app/src/main/java/com/example/futuramaproject/components/ImageComponents.kt
+++ app/src/main/java/com/example/futuramaproject/components/ImageComponents.kt
@@ -2,6 +2,7 @@ package com.example.futuramaproject.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -11,12 +12,17 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun LoadImage(imageResId: Int, size: Dp = 100.dp, onClick: (() -> Unit)? = null) {
+fun LoadImage(
+    imageResId: Int,
+    isFillMaxSize: Boolean = false,
+    size: Dp = 100.dp,
+    onClick: (() -> Unit)? = null
+) {
     Image(
         painter = painterResource(id = imageResId),
         contentDescription = null,
         modifier = Modifier
-            .size(size)
+            .then(if (isFillMaxSize) Modifier.fillMaxSize() else Modifier.size(size))
             .pointerInput(Unit) {
                 detectTapGestures(onTap = { onClick?.invoke() })
             }
--- app/src/main/java/com/example/futuramaproject/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/futuramaproject/screens/home/HomeScreen.kt
@@ -5,22 +5,32 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.futuramaproject.R
-import com.example.futuramaproject.Screen
 import com.example.futuramaproject.components.LoadImage
 
 @Composable
@@ -43,6 +53,8 @@ fun AppBar() {
 
 @Composable
 fun ContentPage(navHostController: NavHostController, paddingValues: PaddingValues) {
+    var showDialog by rememberSaveable { mutableStateOf(false) }
+
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -51,8 +63,35 @@ fun ContentPage(navHostController: NavHostController, paddingValues: PaddingValu
         contentAlignment = Alignment.Center
     ) {
         LoadImage(imageResId = R.drawable.futurama, size = 200.dp, onClick = {
-            navHostController.navigate(Screen.Detail.route)
+            showDialog = true
         })
+
+        if (showDialog) {
+            FullScreenImageDialog(onDismiss = { showDialog = false })
+        }
+    }
+}
+
+@Composable
+fun FullScreenImageDialog(onDismiss: () -> Unit) {
+    Dialog(
+        onDismissRequest = onDismiss, properties = DialogProperties(usePlatformDefaultWidth = false)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Black)
+        ) {
+            LoadImage(
+                imageResId = R.drawable.futurama, isFillMaxSize = true
+            )
+            IconButton(modifier = Modifier
+                .align(Alignment.TopEnd)
+                .padding(top = 40.dp),
+                onClick = { onDismiss() }) {
+                Icon(imageVector = Icons.Default.Clear, contentDescription = "")
+            }
+        }
     }
 }
 
